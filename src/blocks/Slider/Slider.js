8b504083194d6258549f64fb86d7b59b013922b3
Atsuta Roman <indifferentrun@gmail.com>
2019-05-15 17:09:41 +0700

    implement styles for Slider

--- src/blocks/Slider/Slider.js
+++ src/blocks/Slider/Slider.js
@@ -3,8 +3,13 @@ import $ from "jquery"
 $(document).ready(function () {
     var slider = $(".slider");
     console.log(slider.find(".slider__control"));
-});
 
-$(".slider__control").slider({
-    range: true,
+    $(".slider__control").slider({
+        min: 0,
+        max: 15000,
+        step: 100,
+        range: true,
+        values: [5000, 10000],
+        animate: "fast",
+    });
 });
--- src/common.js
+++ src/common.js
@@ -1,24 +1,30 @@
 import "jquery"
+//slider
 import "jquery-ui/ui/widgets/slider"
-import "../vendor/jquery.maskedinput/src/jquery.maskedinput"
-
 import "jquery-ui/themes/base/slider.css"
-
-import "b:text m:type=label-CTA|itemTitle|regular|widgetTitle"
+//masked input
+import "../vendor/jquery.maskedinput/src/jquery.maskedinput"
 
 import "./styles.scss"
 import "./globals/fonts.scss"
 
+import "b:text m:type=label-CTA|itemTitle|regular|widgetTitle"
+
+//BLOCKS
+
+//button
 import "b:button m:type=bordered|filled|text"
 import "b:button m:hovered"
 import "b:button m:width=wide"
-
+//link
 import "b:link m:hovered"
-
+//input
 import "./blocks/Input/_masked/input_masked.scss"
 import "./blocks/Input/_masked/input_masked.js" //импорт с бэм-лоадера даёт ошибку
-
+//text field
 import "b:textField"
-
+//slider
 import "./blocks/Slider/Slider.js"
 import "b:slider"
+
+//----
