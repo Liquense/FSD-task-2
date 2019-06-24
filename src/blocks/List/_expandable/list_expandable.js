f05c6ac4747282d48ec53e02836defc9a5870d8c
Atsuta Roman <indifferentrun@gmail.com>
2019-06-25 03:42:36 +0700

    add expandable option to List

---
+++ src/blocks/List/_expandable/list_expandable.js
@@ -0,0 +1,15 @@
+import "jquery-ui/ui/effects/effect-fade"
+import "jquery-ui/ui/effects/effect-puff"
+import "./list_expandable.scss"
+
+$(".list_expandable").each(function () {
+    let expandableTitle = $(this).find(".list__title_expandable");
+    let expandableContainer = $(this).find(".list__container_expandable");
+
+    $(expandableTitle).click(function () {
+        $(this).toggleClass("list__expandArrow_expanded");
+
+        $(expandableContainer).toggle("fade", [], 500);
+        $(expandableContainer).toggleClass("list__container_visible");
+    });
+});
--- src/blocks/List/list.js
+++ src/blocks/List/list.js
@@ -1,7 +1,8 @@
+import "./list.pug"
 import "./List.scss"
 
 import "./_type/_radio/List_type_radio.scss"
-import "./_type/_radio/List_type_radio.pug"
 
 import "./_type/_checkbox/List_type_checkbox.scss"
-import "./_type/_checkbox/List_type_checkbox.pug"
+
+import "./_expandable/list_expandable.js"
