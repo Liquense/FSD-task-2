a0ce59051ee3f5ce767fb20b1a89c06667c802b0
Atsuta Roman <indifferentrun@gmail.com>
2019-09-13 13:56:05 +0700

    refactor border of button and rename width modifier to size

--- src/blocks/Button/button.js
+++ src/blocks/Button/button.js
@@ -4,10 +4,10 @@ import "./_type/Button_type_bordered.scss"
 import "./_type/Button_type_filled.scss"
 import "./_type/Button_type_text.scss"
 
-import "./_width/Button_width_wide.scss"
+import "./_size/Button_size_wide.scss"
 
 import "./_hovered/Button_hovered.scss"
 
 import "./_decoration/_enterArrow/button_decoration_enterArrow.scss"
 
-import "./_textSized/button_textSized.scss"
+import "./_size/button_textSized.scss"
