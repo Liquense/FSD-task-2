b11fb5e635a62ca88734cbc1e87e3f6d4271ed27
Atsuta Roman <indifferentrun@gmail.com>
2019-09-30 21:06:33 +0700

    add styles to search room page

--- src/sitePages/searchRoom/searchRoom.js
+++ src/sitePages/searchRoom/searchRoom.js
@@ -1,11 +1,10 @@
 import "../../common"
-import "./searchRoom.scss"
 import "../../blocks/Slider/slider"
 import "../../blocks/List/list"
 import "../../blocks/Pagination/Pagination"
 import "../../Cards/roomPreviewCard/roomPreviewCard"
 
 import "./searchRoom.pug"
-
+import "./searchRoom.scss"
 
 
