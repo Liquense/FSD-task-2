503ccdde5e0db00f7cb426227be0e451e595c8f0
Atsuta Roman <indifferentrun@gmail.com>
2019-09-30 14:04:49 +0700

    add function to get full HTML of jquery element

--- src/common/functions.js
+++ src/common/functions.js
@@ -51,3 +51,7 @@ export function ruDeclination(number, words) {
         n = Math.abs(number * 1); // abs на случай отрицательного значения
     return n % 10 == 1 && n % 100 != 11 ? w[0] + w[1] : (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? w[0] + w[2] : w[0] + w[3]);
 }
+
+jQuery.fn.outerHTML = function() {
+    return jQuery('<div />').append(this.eq(0).clone()).html();
+};
