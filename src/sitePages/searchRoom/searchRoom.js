c41950596d8d77d42d118ed22d93222d1271297b
Atsuta Roman <indifferentrun@gmail.com>
2019-09-30 16:55:59 +0700

    implement search room page

--- src/sitePages/searchRoom/searchRoom.js
+++ src/sitePages/searchRoom/searchRoom.js
@@ -1,1 +1,11 @@
+import "../../common"
 import "./searchRoom.scss"
+import "../../blocks/Slider/slider"
+import "../../blocks/List/list"
+import "../../blocks/Pagination/Pagination"
+import "../../Cards/roomPreviewCard/roomPreviewCard"
+
+import "./searchRoom.pug"
+
+
+
