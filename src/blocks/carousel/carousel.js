ba7e84e2b0ca1af943d834df8fe056830359e113
Atsuta Roman <indifferentrun@gmail.com>
2019-09-30 16:22:44 +0700

    refactor carousel

--- src/blocks/carousel/carousel.js
+++ src/blocks/carousel/carousel.js
@@ -4,17 +4,30 @@ import "../../../node_modules/slick-carousel/slick/slick.css"
 import "./carousel.scss"
 
 $(document).ready(function () {
-	$('.carousel').each(function () {
-		const $carousel = $(this);
-
-		const params = {
-			arrows: ($carousel.attr("data-arrows").toLowerCase() === 'true'),
-			prevArrow: '<label class="slick-prev"><button type="button" >expand_more</button></label>',
-			nextArrow: '<label class="slick-next"><button type="button" >expand_more</button></label>',
-			dots: ($carousel.attr("data-dots").toLowerCase() === 'true'),
-		};
-
-		console.log(params);
-		$carousel.slick(params);
-	})
+	initCarouselPlugin($("body"))
 });
+
+function getCarouselParams($carousel) {
+	return {
+		arrows: ($carousel.attr("data-arrows").toLowerCase() === 'true'),
+		prevArrow: '<label class="slick-prev"><button type="button" >expand_more</button></label>',
+		nextArrow: '<label class="slick-next"><button type="button" >expand_more</button></label>',
+		dots: ($carousel.attr("data-dots").toLowerCase() === 'true'),
+	};
+}
+
+export function initCarouselPlugin($parent) {
+	$parent.find(".carousel").each(function () {
+		const initAttrName = "data-initiated";
+		const initAttrValue = "true";
+
+		const $carousel = $(this);
+		if ($carousel.attr(initAttrName) === initAttrValue)
+			return;
+
+		const params = getCarouselParams($carousel);
+
+		$carousel.slick(params);
+		$carousel.attr(initAttrName, initAttrValue);
+	});
+}
