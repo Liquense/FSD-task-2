114130c72e18d4e85a05d91c51718bb58d3e1666
Atsuta Roman <indifferentrun@gmail.com>
2019-09-30 16:28:58 +0700

    separate mixin parameters and add info about them

--- src/Cards/roomPreviewCard/roomPreviewCard.js
+++ src/Cards/roomPreviewCard/roomPreviewCard.js
@@ -1,22 +1,28 @@
 import "./roomPreviewCard.scss"
+import "../../blocks/carousel/carousel"
+import "../../blocks/RateButton/RateButton"
+
 import {formatNumber, ruDeclination} from "../../common/functions";
+import {initCarouselPlugin} from "../../blocks/carousel/carousel";
 
-$('.roomPreviewCard').each(function () {
-	const $roomPreviewCard = $(this);
-	const $costPerPeriodSpan = $roomPreviewCard.find('.roomPreviewCard__costPerPeriod');
-	const $reviewsCountSpan = $roomPreviewCard.find('.roomPreviewCard__reviewsCount');
-	const $reviewsTextSpan = $roomPreviewCard.find('.roomPreviewCard__reviewsText');
+$(document).ready(function () {
+	$('.roomPreviewCard').each(function () {
+		const $roomPreviewCard = $(this);
+		const $costPerPeriodSpan = $roomPreviewCard.find('.roomPreviewCard__costPerPeriod');
+		const $reviewsCountSpan = $roomPreviewCard.find('.roomPreviewCard__reviewsCount');
+		const $reviewsTextSpan = $roomPreviewCard.find('.roomPreviewCard__reviewsText');
 
-	const cardData = {
-		currency: $roomPreviewCard.attr("data-currency"),
-		costPerPeriod: $roomPreviewCard.attr("data-costPerPeriod"),
-		reviewsCount: $roomPreviewCard.attr("data-reviewsCount")
-	};
+		const cardData = {
+			currency: $roomPreviewCard.attr("data-currency"),
+			costPerPeriod: $roomPreviewCard.attr("data-costPerPeriod"),
+			reviewsCount: $roomPreviewCard.attr("data-reviewsCount")
+		};
 
-	const formattedCostPerPeriod = formatNumber(cardData.costPerPeriod, " ");
-	$costPerPeriodSpan.text(formattedCostPerPeriod + cardData.currency);
-	const formattedReviewsCount = formatNumber(cardData.reviewsCount, " ");
-	$reviewsCountSpan.text(formattedReviewsCount);
-	const inclinedReviewsText = ruDeclination(cardData.reviewsCount, "отзыв||а|ов");
-	$reviewsTextSpan.text(inclinedReviewsText);
+		const formattedCostPerPeriod = formatNumber(cardData.costPerPeriod, " ");
+		$costPerPeriodSpan.text(formattedCostPerPeriod + cardData.currency);
+		const formattedReviewsCount = formatNumber(cardData.reviewsCount, " ");
+		$reviewsCountSpan.text(formattedReviewsCount);
+		const inclinedReviewsText = ruDeclination(cardData.reviewsCount, "отзыв||а|ов");
+		$reviewsTextSpan.text(inclinedReviewsText);
+	});
 });
