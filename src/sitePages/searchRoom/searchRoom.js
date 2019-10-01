a2071565591650ad53342e8f3535c55d99d9e9a3
Atsuta Roman <indifferentrun@gmail.com>
2019-10-01 17:55:09 +0700

    add responsiveness

--- src/sitePages/searchRoom/searchRoom.js
+++ src/sitePages/searchRoom/searchRoom.js
@@ -9,6 +9,7 @@ import "./searchRoom.scss"
 
 import {initRoomPreviewCard} from "../../Cards/roomPreviewCard/roomPreviewCard";
 
+//ON PAGE LOAD
 $(".pagination").each(function () {
 	const $paginationBlock = $(this);
 	const $paginationContent = $paginationBlock.children(".pagination__contentContainer");
@@ -19,9 +20,41 @@ $(".pagination").each(function () {
 	})
 });
 
+$(".searchRoom__showSidebarButton").each(function () {
+	const $showSideBarButton = $(this);
+	const $nextElement = $showSideBarButton.next();
+
+	$showSideBarButton.click(function () {
+		$showSideBarButton.toggleClass("searchRoom__showSidebarButton_active");
+	});
+
+	//air-datepicker не относится к контейнеру, где лежит элемент, его инициирующий, из-за этого проблема с "кликом снаружи"
+	//documentAddOnClickFilterCheck($showSideBarButton, $nextElement);
+});
+
+function documentAddOnClickFilterCheck($showSidebarButton, $filterContainer) {
+	$(document).click(function (event) {
+		const $clickedElement = $(event.target);
+
+		if (
+			!(
+				($.contains($filterContainer.get(0), $clickedElement.get(0)))
+				|| ($filterContainer.get(0) === $clickedElement.get(0))
+				|| ($showSidebarButton.get(0) === $clickedElement.get(0))
+			)
+		) {
+			console.log("hmm");
+			$showSidebarButton.removeClass("searchRoom__showSidebarButton_active");
+		}
+	});
+}
+
+//FUNCTIONS
+
 function initAllRoomPreviewCardsInContainer($container) {
 	$container.find(".roomPreviewCard").each(function () {
 			initRoomPreviewCard($(this));
 		}
 	);
 }
+
