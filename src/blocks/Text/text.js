050690e8502c8a6f9038a55fb57965e1dcec011a
Atsuta Roman <indifferentrun@gmail.com>
2020-04-30 00:18:20 +0700

    ~fix ESLint problems in Texts

--- src/blocks/Text/text.js
+++ src/blocks/Text/text.js
@@ -1,4 +1,4 @@
-import "./_type/text_type_itemTitle.scss"
-import "./_type/text_type_label-CTA.scss"
-import "./_type/text_type_regular.scss"
-import "./_type/text_type_widgetTitle.scss"
+import './_type/text_type_itemTitle.scss';
+import './_type/text_type_label-CTA.scss';
+import './_type/text_type_regular.scss';
+import './_type/text_type_widgetTitle.scss';
