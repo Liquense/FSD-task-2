9b7163dc9f7f6f056ce4f6a2d9af00f56d35a18b
Atsuta Roman <indifferentrun@gmail.com>
2020-04-29 21:46:57 +0700

    ~fix ESLint problems in Lists

--- src/blocks/List/_expandable/list_expandable.js
+++ src/blocks/List/_expandable/list_expandable.js
@@ -1,14 +1,20 @@
-import "jquery-ui/ui/effects/effect-fade"
-import "./list_expandable.scss"
+/* eslint-disable import/no-extraneous-dependencies,no-undef */
+// jquery объявлена глобально вебпаком
+import 'jquery-ui/ui/effects/effect-fade';
+import './list_expandable.scss';
 
-$(".list_expandable").each(function () {
-    let expandableTitle = $(this).find(".list__title_expandable");
-    let expandableContainer = $(this).find(".list__container_expandable");
+const $expandableLists = $('.list_expandable');
+function initExpandableList() {
+  const expandableTitle = $(this).find('.list__title_expandable');
+  const expandableContainer = $(this).find('.list__container_expandable');
 
-    $(expandableTitle).click(function () {
-        $(this).toggleClass("list__expandArrow_expanded");
+  function handleExpandableTitleClick() {
+    $(this).toggleClass('list__expandArrow_expanded');
+    $(expandableContainer).toggle('fade', [], 200);
+    $(expandableContainer).toggleClass('list__container_visible');
+  }
 
-        $(expandableContainer).toggle("fade", [], 200);
-        $(expandableContainer).toggleClass("list__container_visible");
-    });
-});
+  $(expandableTitle).click(handleExpandableTitleClick);
+}
+
+$expandableLists.each(initExpandableList);
