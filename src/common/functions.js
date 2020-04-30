3fd526b3284597cb433a20d09080e258df7fd7c3
Atsuta Roman <indifferentrun@gmail.com>
2020-04-30 20:09:22 +0700

    ~fix ESLint problems in common functions

--- src/common/functions.js
+++ src/common/functions.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+// jquery объявлена вебпаком
 /**
  * добавляет пробелы на каждом третьем разряде числа
  * @param number форматируемое число
@@ -5,37 +7,27 @@
  * @returns {string} итоговое число в виде строки
  */
 export function formatNumber(number, symbol) {
-	if (isNaN(number*1)) {
-		return "not number";
-	}
+  if (Number.isNaN(number * 1)) {
+    return 'not number';
+  }
 
-	let stringNum = number.toString();
-	let formattedNum = [];
+  const stringNum = number.toString();
+  const formattedNum = [];
 
-	for (let i = stringNum.length - 1; i >= 0; i--) {
-		if (((stringNum.length - i) % 3) === 0 && (stringNum.length - i) > 0) {
-			formattedNum[i] = `${symbol}${stringNum[i]}`;
-		} else {
-			formattedNum[i] = stringNum[i];
-		}
-	}
+  for (let i = stringNum.length - 1; i >= 0; i -= 1) {
+    if (((stringNum.length - i) % 3) === 0 && (stringNum.length - i) > 0) {
+      formattedNum[i] = `${symbol}${stringNum[i]}`;
+    } else {
+      formattedNum[i] = stringNum[i];
+    }
+  }
 
-	return formattedNum.join("");
+  return formattedNum.join('');
 }
 
 export function compareDateArrays(first, second) {
-	let result = true;
-
-	if (first.length !== second.length) return false;
-
-	for (let i = 0; i < first.length; i++) {
-		if (first[i].getTime() !== second[i].getTime()) {
-			result = false;
-			break;
-		}
-	}
-
-	return result;
+  if (first.length !== second.length) return false;
+  return !first.some((value, index) => value.getTime() !== second[index]?.getTime());
 }
 
 /**
@@ -45,23 +37,32 @@ export function compareDateArrays(first, second) {
  * @author Павел Белоусов <pafnuty10@gmail.com>
  *
  * @param      {number}  number  Число, для которого будет расчитано окончание
- * @param      {string}  words   Слово и варианты окончаний для 1|2|100 (1 комментарий, 2 комментария, 100 комментариев)
+ * @param      {string}  words   Слово и варианты окончаний для 1|2|100
+ * (1 комментарий, 2 комментария, 100 комментариев)
  * @return     {string}  Cлово с правильным окончанием
  */
 export function ruDeclination(number, words) {
-	'use strict';
-	let w = words.split('|'),
-		n = Math.abs(number * 1); // abs на случай отрицательного значения
-	return n % 10 == 1 && n % 100 != 11 ? w[0] + w[1] : (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? w[0] + w[2] : w[0] + w[3]);
+  const w = words.split('|');
+  const n = Math.abs(number); // abs на случай отрицательного значения
+  const firstEndingCondition = n % 10 === 1 && n % 100 !== 11;
+  const secondEndingCondition = n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20);
+  if (firstEndingCondition) {
+    return w[0] + w[1];
+  } if (secondEndingCondition) {
+    return w[0] + w[2];
+  }
+  return w[0] + w[3];
 }
 
 /**
  * Возвращает HTML-код, включая сам объект, а не только его содержимое
  * @returns {jQuery}
  */
-jQuery.fn.outerHTML = function () {
-	return jQuery('<div />').append(this.eq(0).clone()).html();
-};
+function outerHTML() {
+  return jQuery('<div />').append(this.eq(0).clone()).html();
+}
+// eslint-disable-next-line no-undef
+jQuery.fn.outerHTML = outerHTML;
 
 /**
  * Копирует простые объекты (без вложенных ссылочных типов) в новый экземпляр
@@ -69,15 +70,9 @@ jQuery.fn.outerHTML = function () {
  * @returns {[]} новый массив объектов
  */
 export function copyArrayOfObjects(arrayOfObj) {
-	let resultArray = [];
-
-	for (let obj of arrayOfObj) {
-		resultArray.push(Object.assign({}, obj));
-	}
-
-	return resultArray;
+  return arrayOfObj.map((obj) => ({ ...obj }));
 }
 
 export function clamp(value, min, max) {
-	return Math.min(Math.max(value, min), max);
+  return Math.min(Math.max(value, min), max);
 }
