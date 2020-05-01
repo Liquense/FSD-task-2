f3a8fd45a82404ab908fb3f019e232255d819e27
Atsuta Roman <indifferentrun@gmail.com>
2020-05-01 21:29:21 +0700

    +create files for form-elements section of UI-kit page

--- src/sitePages/ui-kit/ui-kit.js
+++ src/sitePages/ui-kit/ui-kit.js
@@ -1,26 +1,15 @@
 import 'jquery-ui/ui/widgets/checkboxradio';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import '../../../vendor/jquery.maskedinput/src/jquery.maskedinput';
-
 import './ui-kit.pug';
 import './ui-kit.scss';
 import './ui-kit__colors-type/ui-kit__colors-type.scss';
+import './ui-kit__form-elements/ui-kit__form-elements';
+
 import '../../common';
-import '../../blocks/Button/button';
-import '../../blocks/Input/input';
-import '../../blocks/Slider/sliderDependencies';
-import '../../blocks/twoCalendarRangePicker/twoCalendarRangePicker';
-import '../../blocks/Checkbox/Checkbox';
-import '../../blocks/List/list';
-import '../../blocks/RateButton/RateButton';
-import '../../blocks/Pagination/Pagination';
-import '../../blocks/Comment/Comment';
 import '../../Cards/findRoomCard/findRoomCard';
 import '../../Cards/registrationCard/registrationCard';
 import '../../Cards/bookingCard/bookingCard';
 import '../../Cards/loginCard/loginCard';
 import '../../blocks/carousel/carousel';
 import '../../Cards/roomPreviewCard/roomPreviewCard';
-import '../../pageElements/header/header';
-import '../../pageElements/footer/footer';
---
+++ src/sitePages/ui-kit/ui-kit__form-elements/ui-kit__form-elements.js
@@ -0,0 +1,11 @@
+import '../../../blocks/Button/button';
+import '../../../blocks/Input/input';
+import '../../../blocks/Slider/slider';
+import '../../../blocks/twoCalendarRangePicker/twoCalendarRangePicker';
+import '../../../blocks/Checkbox/Checkbox';
+import '../../../blocks/List/list';
+import '../../../blocks/RateButton/RateButton';
+import '../../../blocks/Pagination/Pagination';
+import '../../../blocks/Comment/Comment';
+
+import './ui-kit__form-elements.scss';
