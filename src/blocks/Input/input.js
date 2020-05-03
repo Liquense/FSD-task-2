03fc4e5f94de4667066fc46a0afdb7605179caac
Atsuta Roman <indifferentrun@gmail.com>
2020-05-04 03:34:31 +0700

    ~rearrange imports

--- src/blocks/Input/input.js
+++ src/blocks/Input/input.js
@@ -11,8 +11,8 @@ import './_width/input_width_medium.scss';
 // ---Type
 import './_type/input_type_text.scss';
 
-import './_type/_datepicker/input_type_datepicker.scss';
 import './_type/_datepicker/input_type_datepicker';
+import './_type/_datepicker/input_type_datepicker.scss';
 
 import './_type/_dropdown/input__list_type_dropdown';
 import './_type/_dropdown/input__list_type_dropdown.scss';
@@ -20,7 +20,6 @@ import './_type/_dropdown/input__list_type_dropdown.scss';
 import './_type/_spinner/input_type_spinner';
 import './_type/_spinner/input_type_spinner.scss';
 
-import './_decoration/_expandArrow/input_decoration_expandArrow.scss';
 import './_decoration/_expandArrow/input_decoration_expandArrow';
+import './_decoration/_expandArrow/input_decoration_expandArrow.scss';
 import './_decoration/_enterArrow/input_decoration_enterArrow.scss';
-
