60bd670dac990ac55ef1ea8e1f4fae8e33a1d63f
Atsuta Roman <indifferentrun@gmail.com>
2020-05-05 01:35:04 +0700

    +implement form-elements page for ui-kit

--- src/sitePages/ui-kit/ui-kit.js
+++ src/sitePages/ui-kit/ui-kit.js
@@ -1,12 +1,6 @@
+import '../../common';
+
 import './ui-kit.pug';
 import './ui-kit.scss';
 import './ui-kit__colors-type/ui-kit__colors-type.scss';
 import './ui-kit__form-elements/ui-kit__form-elements';
-
-import '../../common';
-import '../../Cards/findRoomCard/findRoomCard';
-import '../../Cards/registrationCard/registrationCard';
-import '../../Cards/bookingCard/bookingCard';
-import '../../Cards/loginCard/loginCard';
-import '../../blocks/carousel/carousel';
-import '../../Cards/roomPreviewCard/roomPreviewCard';
--- src/sitePages/ui-kit/ui-kit__form-elements/ui-kit__form-elements.js
+++ src/sitePages/ui-kit/ui-kit__form-elements/ui-kit__form-elements.js
@@ -1,11 +1,33 @@
+/* eslint-disable no-undef */
+// jquery подключена вебпаком
 import '../../../blocks/Button/button';
 import '../../../blocks/Input/input';
 import '../../../blocks/Slider/slider';
-import '../../../blocks/twoCalendarRangePicker/twoCalendarRangePicker';
-import '../../../blocks/Checkbox/Checkbox';
+import { initTwoCalendarPicker } from '../../../blocks/twoCalendarRangePicker/twoCalendarRangePicker';
 import '../../../blocks/List/list';
 import '../../../blocks/RateButton/RateButton';
 import '../../../blocks/Pagination/Pagination';
 import '../../../blocks/Comment/Comment';
+import Checkbox from '../../../blocks/Checkbox/Checkbox';
+import { initDatepickerInput } from '../../../blocks/Input/_type/_datepicker/input_type_datepicker';
+import initDropdownInput from '../../../blocks/Input/_type/_dropdown/input__list_type_dropdown';
 
 import './ui-kit__form-elements.scss';
+import initExpandableList from '../../../blocks/List/_expandable/list_expandable';
+
+Checkbox.initDefault();
+Checkbox.initRadio();
+Checkbox.initToggle();
+Checkbox.initLike();
+
+const $datepickers = $('.input_type_datepicker');
+$datepickers.each(initDatepickerInput);
+
+const $twoCalendarPickers = $('.twoCalendarRangePicker');
+$twoCalendarPickers.each(initTwoCalendarPicker);
+
+const $dropdowns = $('.input_type_dropdown');
+$dropdowns.each(initDropdownInput);
+
+const $expandableLists = $('.list_expandable');
+$expandableLists.each(initExpandableList);
