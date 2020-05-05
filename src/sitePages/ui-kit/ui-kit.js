8682a44187691dbcc1700388ccf975db188edb39
Atsuta Roman <indifferentrun@gmail.com>
2020-05-06 03:48:10 +0700

    +implement cards section for UI-Kit

--- src/sitePages/ui-kit/ui-kit.js
+++ src/sitePages/ui-kit/ui-kit.js
@@ -4,3 +4,4 @@ import './ui-kit.pug';
 import './ui-kit.scss';
 import './ui-kit__colors-type/ui-kit__colors-type.scss';
 import './ui-kit__form-elements/ui-kit__form-elements';
+import './ui-kit__cards/ui-kit__cards';
