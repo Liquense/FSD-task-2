e2c01adc8d48f25472ff08a9c5acd1cd6557e4c2
Atsuta Roman <indifferentrun@gmail.com>
2020-05-06 21:08:11 +0700

    +automatic initialize of datepickers and dropdowns within card

--- src/Cards/findRoomCard/findRoomCard.js
+++ src/Cards/findRoomCard/findRoomCard.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-undef */
+// jquery подключена вебпаком
 import './findRoomCard.scss';
 import '../../blocks/Input/input';
-import '../../blocks/twoCalendarRangePicker/twoCalendarRangePicker';
+import { initTwoCalendarPicker } from '../../blocks/twoCalendarRangePicker/twoCalendarRangePicker';
+import initDropdownInput from '../../blocks/Input/_type/_dropdown/input__list_type_dropdown';
+
+export default function initFindRoomCard(index, cardElement) {
+  const $card = $(cardElement);
+  const $twoCalendarPickers = $card.find('.twoCalendarRangePicker');
+  $twoCalendarPickers.each(initTwoCalendarPicker);
+  const $dropdowns = $card.find('.input_type_dropdown');
+  $dropdowns.each(initDropdownInput);
+}
