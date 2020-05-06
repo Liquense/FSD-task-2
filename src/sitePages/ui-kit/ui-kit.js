6964793ab267e43cb02dccb5a077e8f9fccca891
Atsuta Roman <indifferentrun@gmail.com>
2020-05-06 18:32:58 +0700

    +implement headers-footers section of UI-Kit

--- src/sitePages/ui-kit/ui-kit.js
+++ src/sitePages/ui-kit/ui-kit.js
@@ -5,3 +5,4 @@ import './ui-kit.scss';
 import './ui-kit__colors-type/ui-kit__colors-type.scss';
 import './ui-kit__form-elements/ui-kit__form-elements';
 import './ui-kit__cards/ui-kit__cards';
+import './ui-kit__headers-footers/ui-kit__headers-footers';
