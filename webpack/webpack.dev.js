7468466a8f538c92e8e95b5f222d0fafe83e9d9c
Atsuta Roman <indifferentrun@gmail.com>
2020-05-10 01:56:23 +0700

    ~merge from master & update gh-pages

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,17 +1,19 @@
 const webpack = require('webpack');
 const path = require('path');
-let rootPath = path.join(__dirname, '../');
+
+const rootPath = path.join(__dirname, '../');
 const merge = require('webpack-merge');
-const common = require(rootPath + '/webpack/webpack.config');
+
+const common = require(`${rootPath}/webpack/webpack.config`);
 
 module.exports = merge(common, {
-   mode: 'development',
-   devServer: {
-       hot: true,
-       compress: true,
-       port: 8080
-   },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ],
+  mode: 'development',
+  devServer: {
+    hot: true,
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 });
