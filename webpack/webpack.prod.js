b22521cfc9bb02997d0633555fe7108fe27982d9
Atsuta Roman <indifferentrun@gmail.com>
2020-05-13 23:18:26 +0700

    ~fix webpack configs for production building

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,17 +1,16 @@
+/* eslint-disable */
 const webpack = require('webpack');
 const path = require('path');
-let rootPath = path.join(__dirname, '../');
+
+const rootPath = path.join(__dirname, '../');
 const merge = require('webpack-merge');
-const common = require(rootPath + '/webpack/webpack.config');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const common = require(`${rootPath}/webpack/webpack.config`);
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(common, {
-	mode: 'production',
-	plugins: [
-		new BundleAnalyzerPlugin()
-	],
-	module: {
-		rules: [
-		]
-	}
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin(),
+  ],
 });
