34498ce641b5f1fc9ac6a37c35905687d9acd1b9
Atsuta Roman <indifferentrun@gmail.com>
2020-05-14 18:43:04 +0700

    ~fix improper creation of masked input

--- src/cards/registration-card/registration-card.js
+++ src/cards/registration-card/registration-card.js
@@ -1,6 +1,7 @@
 import './registration-card.scss';
 import Checkbox from '../../blocks/checkbox/checkbox';
 import '../../blocks/list/list';
+import '../../blocks/input/input';
 
 export default function initRegistrationCard() {
   Checkbox.initRadio();
