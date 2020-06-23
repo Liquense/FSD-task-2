07af8e15fef80ec1d1e3b383da032667ec1ac87e
Atsuta Roman <indifferentrun@gmail.com>
2020-06-23 13:55:49 +0700

    ~update footers due to #4 and #6

--- src/page-elements/footer/footer.js
+++ src/page-elements/footer/footer.js
@@ -1,5 +1,10 @@
 import '../../blocks/input/input';
+
 import './footer.scss';
+
+import './__socialMedia/footer__socialMedia.scss';
+
 import './_type/_large/footer_type_large.scss';
 import './_type/_simple/footer_type_simple.scss';
+
 import '@fortawesome/fontawesome-free/css/all.css';
