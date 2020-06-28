13b935d34ac63061c2afb93e04513151cc575c6b
Atsuta Roman <indifferentrun@gmail.com>
2020-06-28 21:02:37 +0700

    +auto import functional #2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import './common';
-import './index.scss';
+export function importContext(r) {
+  r.keys().forEach(r);
+}
+
+export function importCommon() {
+  importContext(require.context('./', false, /index\.scss$/));
+  importContext(require.context('./blocks', true, /\.(js|scss)$/));
+  importContext(require.context('./common', true, /\.(js|scss)$/));
+  importContext(require.context('./cards', true, /\.(js|scss)$/));
+  importContext(require.context('./page-elements', true, /\.(js|scss)$/));
+}
