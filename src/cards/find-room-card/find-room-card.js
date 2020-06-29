e82229db62d591f385bd35d2c062768141a44dbc
Atsuta Roman <indifferentrun@gmail.com>
2020-06-29 21:06:13 +0700

    ~refactor dropdowns initialization

--- src/cards/find-room-card/find-room-card.js
+++ src/cards/find-room-card/find-room-card.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-undef */
 // jquery подключена вебпаком
 import { initTwoCalendarPicker } from '../../blocks/two-calendar-range-picker/two-calendar-range-picker';
-import { initDropdown } from '../../blocks/dropdown/dropdown';
+import { initDropdowns } from '../../blocks/dropdown/dropdown';
 
 export default function initFindRoomCard(index, cardElement) {
   const $card = $(cardElement);
   const $twoCalendarPickers = $card.find('.two-calendar-range-picker');
   $twoCalendarPickers.each(initTwoCalendarPicker);
-  const $dropdowns = $card.find('.input_type_dropdown');
-  $dropdowns.each(initDropdown);
+  initDropdowns();
 }
