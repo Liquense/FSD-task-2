4c204d4c117a3549bde7e0326039351430b41c26
Atsuta Roman <indifferentrun@gmail.com>
2020-06-29 22:10:36 +0700

    ~refactor footer to flat file structure #5

--- src/page-elements/footer/footer.js
+++ src/page-elements/footer/footer.js
@@ -1,10 +1,1 @@
-import '../../blocks/input/input';
-
-import './footer.scss';
-
-import './__socialMedia/footer__socialMedia.scss';
-
-import './_type/_large/footer_type_large.scss';
-import './_type/_simple/footer_type_simple.scss';
-
 import '@fortawesome/fontawesome-free/css/all.css';
