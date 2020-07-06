3d6467b690353dc7f4a4463e62ea0d9103fb850f
Atsuta Roman <indifferentrun@gmail.com>
2020-07-06 21:02:27 +0700

    +add 'js-' prefix in header #10 ~replace double quotes with single ~replace concatenations with template strings #8 -remove mixes #3

--- src/site-pages/registration-login/registration-login.js
+++ src/site-pages/registration-login/registration-login.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 // jquery импортирована вебпаком
 import { importCommon, importContext } from '../../index';
-import initLoginCards from "../../cards/login-card/login-card";
+import initLoginCards from '../../cards/login-card/login-card';
 
 importCommon();
 importContext(require.context('./', true, /\.(js|scss)$/));
@@ -11,21 +11,14 @@ const isLogin = url.searchParams.get('login');
 
 const template = require('./registration-login__card-template.pug');
 
-const locals = {
-  login: isLogin,
-};
+const locals = { login: isLogin === 'true' };
 const templateHTML = template(locals);
 
-const $registrationLoginContent = $('.registration-login__content');
-
+const $registrationLoginContent = $('.js-registration-login__content');
 if (locals.login === 'true') {
-  $registrationLoginContent.addClass('registration-login__login-container')
-    .html(templateHTML);
+  $registrationLoginContent.append(templateHTML);
 } else {
-  const $footer = $('.registration-login__footer');
-  $footer.addClass('registration-login__registration-footer');
-  $registrationLoginContent.addClass('registration-login__registration-container')
-    .html(templateHTML);
+  $registrationLoginContent.append(templateHTML);
 }
 
 initLoginCards();
