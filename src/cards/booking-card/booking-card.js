f6bbab0637ab9b7e1902b9eff04b3c28ab9e6f65
Atsuta Roman <indifferentrun@gmail.com>
2020-07-06 16:05:29 +0700

    ~destruct params in booking-card #6 +add 'js-' prefix #10 -remove mixes #3

--- src/cards/booking-card/booking-card.js
+++ src/cards/booking-card/booking-card.js
@@ -75,8 +75,8 @@ function addRefreshCheckOnInputChange(
   $firstDatePicker, $secondDatePicker, $stayingCostRow, $totalCostSpan, priceData,
 ) {
   function refreshCheckValuesOnDateChange(event) {
-    const $calculatingStayingCostSpan = $stayingCostRow.children('.booking-card__staying-cost-calculation');
-    const $stayingSumSpan = $stayingCostRow.children('.booking-card__staying-cost-sum');
+    const $calculatingStayingCostSpan = $stayingCostRow.children('.js-booking-card__staying-cost-calculation');
+    const $stayingSumSpan = $stayingCostRow.children('.js-booking-card__staying-cost-sum');
     const datePickerData = $(event.target).data('datepicker');
     const daysCount = getDaysFromDateRange(datePickerData.selectedDates);
 
@@ -96,19 +96,19 @@ function addRefreshCheckOnInputChange(
 function initBookingCard() {
   const $bookingCard = $(this);
 
-  const $dailyPrice = $bookingCard.find('.booking-card__daily-price');
+  const $dailyPrice = $bookingCard.find('.js-booking-card__daily-price');
   const priceAmount = $dailyPrice.attr('data-dailyPrice');
   const currency = $dailyPrice.attr('data-currency');
   const priceToShow = formatNumber(priceAmount, ' ');
   writeFormattedDailyPrice($dailyPrice, priceToShow, currency);
 
-  const $rangePicker = $bookingCard.find('.booking-card__range-picker > .two-calendar-range-picker');
-  const $firstDatepicker = $($rangePicker.find('.two-calendar-range-picker__first-datepicker')[0]);
-  const $firstDatepickerControl = $($firstDatepicker.find('.datepicker-block .input__control')[0]);
-  const $secondDatepicker = $($rangePicker.find('.two-calendar-range-picker__second-datepicker')[0]);
-  const $secondDatepickerControl = $($secondDatepicker.find('.datepicker-block .input__control')[0]);
+  const $rangePicker = $bookingCard.find('.js-booking-card__range-picker > .js-two-calendar-range-picker');
+  const $firstDatepicker = $($rangePicker.find('.js-two-calendar-range-picker__first-datepicker')[0]);
+  const $firstDatepickerControl = $($firstDatepicker.find('.js-datepicker-block .js-input__control')[0]);
+  const $secondDatepicker = $($rangePicker.find('.js-two-calendar-range-picker__second-datepicker')[0]);
+  const $secondDatepickerControl = $($secondDatepicker.find('.js-datepicker-block .js-input__control')[0]);
 
-  const $stayingCostRow = $bookingCard.find('.booking-card__staying-cost-row');
+  const $stayingCostRow = $bookingCard.find('.js-booking-card__staying-cost-row');
 
   const priceData = {
     price: priceAmount,
@@ -119,7 +119,7 @@ function initBookingCard() {
     stayingSum: 0,
   };
 
-  const $totalCostSpan = $bookingCard.find('.booking-card__summary-total-cost');
+  const $totalCostSpan = $bookingCard.find('.js-booking-card__summary-total-cost');
   addRefreshCheckOnInputChange(
     $firstDatepickerControl,
     $secondDatepickerControl,
@@ -130,11 +130,11 @@ function initBookingCard() {
 
   setInitialDates($rangePicker, $firstDatepickerControl);
 
-  const $servicesEnumSpan = $bookingCard.find('.booking-card__services');
-  const $servicesSumSpan = $bookingCard.find('.booking-card__services-sum');
+  const $servicesEnumSpan = $bookingCard.find('.js-booking-card__services');
+  const $servicesSumSpan = $bookingCard.find('.js-booking-card__services-sum');
 
   const servicesData = getOverallServicesData($servicesEnumSpan, $servicesSumSpan, priceData);
-  const $addServicesSumSpan = $bookingCard.find('.booking-card__additional-services-sum');
+  const $addServicesSumSpan = $bookingCard.find('.js-booking-card__additional-services-sum');
   priceData.addServicesSum = $addServicesSumSpan.attr('data-addServices');
 
   writeServicesToSpans($servicesEnumSpan, $servicesSumSpan, currency, servicesData);
@@ -142,7 +142,7 @@ function initBookingCard() {
 }
 
 function initBookingCards() {
-  const $bookingCards = $('.booking-card');
+  const $bookingCards = $('.js-booking-card');
   $bookingCards.each(initBookingCard);
 }
 
