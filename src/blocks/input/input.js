0801f33d89d001be8b886702ba666371a174fcd3
Atsuta Roman <indifferentrun@gmail.com>
2020-07-18 01:09:00 +0700

    ~refactor input into class #11

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -2,24 +2,32 @@
 // jquery объявлена глобально вебпаком
 import 'jquery.maskedinput/src/jquery.maskedinput';
 
-// region Masked
-function initMaskedInputs() {
-  const $document = $(document);
-  $document.ready(($) => {
+class MaskedInput {
+  $maskedInput;
+
+  placeholder;
+
+  constructor(rootElement) {
+    this._initProps(rootElement);
+    this._initPlugin();
+  }
+
+  _initProps(rootElement) {
+    this.$maskedInput = $(rootElement).find('.js-input__control_type_masked');
+    this.placeholder = this.$maskedInput.attr('placeholder');
+  }
+
+  _initPlugin() {
     $.mask.definitions.D = '[0-3]';
     $.mask.definitions.M = '[0-1]';
     $.mask.definitions.Y = '[1-2]';
 
-    const $maskedInput = $('.js-input__control_type_masked');
-    const placeholder = $maskedInput.attr('placeholder');
-
-    $maskedInput.mask('D9.M9.Y999',
+    this.$maskedInput.mask('D9.M9.Y999',
       {
-        placeholder,
+        placeholder: this.placeholder,
         autoclear: false,
       });
-  });
+  }
 }
-// endregion
 
-export default initMaskedInputs;
+export default MaskedInput;
