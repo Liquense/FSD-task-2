c356e2e24bc9987caa1e3adb720a788e76876f64
Atsuta Roman <indifferentrun@gmail.com>
2020-07-18 01:08:08 +0700

    ~refactor checkbox into class #11

--- src/blocks/checkbox/checkbox.js
+++ src/blocks/checkbox/checkbox.js
@@ -2,99 +2,93 @@
 // jQuery объявлена глобально вебпаком
 import 'jquery-ui/ui/widgets/checkboxradio';
 
-export function initCheckbox(jquerySelector, { icon, iconSpace }) {
-  const $hiddenInput = $(jquerySelector);
+class Checkbox {
+  static defaultClass = 'checkbox_type_default';
 
-  function initialization() {
-    const $singleInput = $(this);
-    if ($singleInput.data('uiCheckboxradio')) return; // чтобы не инициализировать лишнего
+  static likeClass = 'checkbox_type_like';
 
-    const checkbox = $singleInput.checkboxradio({
+  static radioClass = 'checkbox_type_radio';
+
+  static toggleClass = 'checkbox_type_toggle';
+
+  $checkbox;
+
+  $pluginElement;
+
+  constructor(rootElement) {
+    this._initElement(rootElement);
+    this._initCheckbox();
+  }
+
+  _initElement(rootElement) {
+    this.$checkbox = $(rootElement);
+  }
+
+  _initCheckbox() {
+    if (this.$checkbox.hasClass(Checkbox.defaultClass)) this._initDefault(); else
+    if (this.$checkbox.hasClass(Checkbox.radioClass)) this._initRadio(); else
+    if (this.$checkbox.hasClass(Checkbox.likeClass)) this._initLike(); else
+    if (this.$checkbox.hasClass(Checkbox.toggleClass)) this._initToggle();
+  }
+
+
+  _initPlugin(icon, iconSpace) {
+    const checkbox = this.$pluginElement.checkboxradio({
       classes: {
         'ui-checkboxradio-icon': icon,
         'ui-checkboxradio-icon-space': iconSpace,
       },
     });
-    const isChecked = $singleInput.attr('data-isChecked');
+    const isChecked = this.$pluginElement.attr('data-isChecked');
 
     if (isChecked === 'true') {
       checkbox.attr('checked', 'checked').change();
     }
   }
 
-  $hiddenInput.each(initialization);
-}
+  _initLike() {
+    const $label = this.$checkbox.find('.js-checkbox__label_type_like');
+    this.$pluginElement = this.$checkbox.find('.js-checkbox__hidden-button_type_like');
+    const $likeText = this.$checkbox.find('.js-checkbox__text_type_like');
 
-function initToggleCheckboxes() {
-  initCheckbox('.js-checkbox__hidden-button_type_toggle', {
-    icon: 'checkbox__button checkbox__button_type_toggle',
-    iconSpace: 'checkbox__icon-space checkbox__icon-space_type_toggle',
-  });
-}
+    this._initPlugin('checkbox__button checkbox__button_type_like');
 
-function initRadioCheckboxes() {
-  initCheckbox('.js-checkbox__hidden-button_type_radio',
-    {
-      icon: 'checkbox__button checkbox__button_type_radio',
-      iconSpace: 'checkbox__icon-space checkbox__icon-space_type_radio',
+    const gradientBorderElement = document.createElement('div');
+    gradientBorderElement.classList.add('checkbox__button-border_type_like');
+    $label.prepend(gradientBorderElement);
+
+    let likesCount = Number.parseInt($label.attr('data-likes-count'), 10);
+    if (Number.isNaN(likesCount)) return;
+
+    this.$pluginElement.change(() => {
+      likesCount = $label.hasClass('ui-checkboxradio-checked') ? likesCount + 1 : likesCount - 1;
+
+      $likeText.text(likesCount);
+      $label.attr('data-likes-count', likesCount);
     });
-}
-
-function initLikeCheckbox() {
-  const $likeLabel = $(this);
-  const $likeButton = $likeLabel.find('.js-checkbox__hidden-button_type_like');
-  const $likeText = $likeLabel.find('.js-checkbox__text_type_like');
-
-  const checkboxRadioData = $likeButton.data('uiCheckboxradio');
-  if (checkboxRadioData) return; // чтобы не навешивать лишних обработчиков
-
-  initCheckbox($likeButton, { icon: 'checkbox__button checkbox__button_type_like' });
-
-  const gradientBorderElement = document.createElement('div');
-  gradientBorderElement.classList.add('checkbox__button-border_type_like');
-  $likeLabel.prepend(gradientBorderElement);
-
-  let likesCount = Number.parseInt($likeLabel.attr('data-likes-count'), 10);
-  if (Number.isNaN(likesCount)) return;
-
-  $likeButton.change(() => {
-    likesCount = $likeLabel.hasClass('ui-checkboxradio-checked') ? likesCount + 1 : likesCount - 1;
-
-    $likeText.text(likesCount);
-    $likeLabel.attr('data-likes-count', likesCount);
-  });
-}
-
-function initLikeCheckboxes() {
-  const $likeCheckboxes = $('.js-checkbox__label_type_like');
-  $likeCheckboxes.each(initLikeCheckbox);
-}
-
-function initDefaultCheckboxes() {
-  initCheckbox('.js-checkbox__hidden-button_type_default', {
-    icon: 'checkbox__button js-checkbox__button_type_default checkbox__button_type_default',
-    iconSpace: 'checkbox__icon-space checkbox__icon-space_type_default',
-  });
-
-  const $defaultCheckboxButtons = $('.js-checkbox__button_type_default');
-  $defaultCheckboxButtons.text('check');
-}
-
-class Checkbox {
-  static initDefault() {
-    initDefaultCheckboxes();
   }
 
-  static initLike() {
-    initLikeCheckboxes();
+  _initDefault() {
+    this.$pluginElement = this.$checkbox.find('.js-checkbox__hidden-button_type_default');
+    this._initPlugin(
+      'checkbox__button js-checkbox__button_type_default checkbox__button_type_default',
+      'checkbox__icon-space checkbox__icon-space_type_default',
+    );
+
+    const $defaultCheckboxButtons = this.$checkbox.find('.js-checkbox__button_type_default');
+    $defaultCheckboxButtons.text('check');
   }
 
-  static initRadio() {
-    initRadioCheckboxes();
+  _initRadio() {
+    this.$pluginElement = this.$checkbox.find('.js-checkbox__hidden-button_type_radio');
+    this._initPlugin('checkbox__button checkbox__button_type_radio',
+      'checkbox__icon-space checkbox__icon-space_type_radio');
   }
 
-  static initToggle() {
-    initToggleCheckboxes();
+  _initToggle() {
+    this.$pluginElement = this.$checkbox.find('.js-checkbox__hidden-button_type_toggle');
+    this._initPlugin('checkbox__button checkbox__button_type_toggle',
+      'checkbox__icon-space checkbox__icon-space_type_toggle');
   }
 }
 
