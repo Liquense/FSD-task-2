89477a631531f2259bb460f6776844ff97a8e72c
Atsuta Roman <indifferentrun@gmail.com>
2020-08-20 20:49:33 +0700

    ~rename init inputs function +add returning of classes instances

--- src/blocks/input/init.js
+++ src/blocks/input/init.js
@@ -1,8 +1,8 @@
 import initBlocks from '../../common/dynamicInit';
 import Input from './input';
 
-function initMaskedInputs(rootElement) {
-  initBlocks(rootElement, '.js-input', Input);
+function initInputs(rootElement) {
+  return initBlocks(rootElement, '.js-input', Input);
 }
 
-export default initMaskedInputs;
+export default initInputs;
--- src/site-pages/ui-kit/ui-kit__form-elements/ui-kit-form-elements.js
+++ src/site-pages/ui-kit/ui-kit__form-elements/ui-kit-form-elements.js
@@ -1,5 +1,5 @@
 import initDropdowns from '../../../blocks/dropdown/init';
-import initMaskedInputs from '../../../blocks/input/init';
+import initInputs from '../../../blocks/input/init';
 import initLists from '../../../blocks/checkbox-list/init';
 import initPaginations from '../../../blocks/pagination/init';
 import initSliders from '../../../blocks/slider/init';
@@ -8,7 +8,7 @@ import initRatings from '../../../blocks/rating/init';
 import initLikes from '../../../blocks/like-button/init';
 
 initDropdowns();
-initMaskedInputs();
+initInputs();
 initLikes();
 initLists();
 initPaginations();
