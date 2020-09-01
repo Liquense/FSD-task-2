367ebed8b991191fd60927fefcc886bc68b26924
Atsuta Roman <indifferentrun@gmail.com>
2020-09-01 23:13:41 +0700

    ~move UI-KIT parts to separate blocks #16 #28

---
+++ src/page-elements/form-elements/form-elements.js
@@ -0,0 +1,17 @@
+import initDropdowns from '../../blocks/dropdown/init';
+import initInputs from '../../blocks/input/init';
+import initLists from '../../blocks/checkbox-list/init';
+import initPaginations from '../../blocks/pagination/init';
+import initSliders from '../../blocks/slider/init';
+import initTwoCalendarDatepickers from '../../blocks/double-date-picker/init';
+import initRatings from '../../blocks/rating/init';
+import initLikes from '../../blocks/like-button/init';
+
+initDropdowns();
+initInputs();
+initLikes();
+initLists();
+initPaginations();
+initSliders();
+initTwoCalendarDatepickers();
+initRatings();
