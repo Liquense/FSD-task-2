4313baab4bd78f57e438dbb419f48096ee2abc54
Atsuta Roman <indifferentrun@gmail.com>
2020-09-22 18:46:40 +0700

    ~refactor #30

--- src/imports.js
+++ src/imports.js
@@ -4,7 +4,7 @@ function importContext(r) {
 
 function importCommon() {
   importContext(require.context('./blocks', true, /\.(js|scss)$/));
-  importContext(require.context('./common', true, /\.(js|scss)$/));
+  importContext(require.context('./common', true, /(.js|fonts.scss)$/));
   importContext(require.context('./cards', true, /\.(js|scss)$/));
   importContext(require.context('./assets/fonts', true, /\.(otf|ttf|svg|woff|woff2|eot)$/));
 }
